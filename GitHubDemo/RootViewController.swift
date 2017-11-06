6967345b4c8e0ade176f106def8d7c8773b0aca7
Jess Telmanik <JessTelmanik@Jesss-MacBook-Pro.local>
2017-11-06 10:50:53 -0500

    Initial Commit

---
+++ GitHubDemo/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  GitHubDemo
+//
+//  Created by Jess Telmanik on 11/6/17.
+//  Copyright © 2017 Jess Telmanik. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
